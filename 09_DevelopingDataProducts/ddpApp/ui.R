faaaf7ef334b848378db79dbfccbf02152957d39
Brian Linn <blinn@calquake.com>
2017-01-27 08:52:07 -0800

    Added ShinyApps and ShinyGadget notes

--- 09_DevelopingDataProducts/ddpApp/ui.R
+++ 09_DevelopingDataProducts/ddpApp/ui.R
@@ -79,25 +79,86 @@ library(shiny)
 #         )
 # ))
 
-#Building the prediction function
+# #Apps with inputs
+# library(shiny)
+# shinyUI(fluidPage(
+#         titlePanel("Plot Random Numbers"),
+#         sidebarLayout(
+#                 sidebarPanel(
+#                         numericInput("numeric", "How Many Random Numbers Should be Plotted?",
+#                                      value = 1000, min = 1, max = 1000, step = 1),
+#                         sliderInput("sliderX", "Pick Minimum and Maximum X Values",
+#                                     -100, 100, value = c(-50, 50)),
+#                         sliderInput("sliderY", "Pick Minimum and Maximum Y Values",
+#                                     -100, 100, value = c(-50, 50)),
+#                         checkboxInput("show_xlab", "Show/Hide X Axis Label", value = TRUE),
+#                         checkboxInput("show_ylab", "Show/Hide Y Axis Label", value = TRUE),
+#                         checkboxInput("show_title", "Show/Hide Title")
+#                 ),
+#                 mainPanel(
+#                         h3("Graph of Random Points"),
+#                         plotOutput("plot1")
+#                 )
+#         )
+# ))
+
+# #Apps with reactive expressions
+# library(shiny)
+# shinyUI(fluidPage(
+#         titlePanel("Predict Horespower from MPG"),
+#         sidebarLayout(
+#                 sidebarPanel(
+#                         sliderInput("sliderMPG", "What is the MPG of the car?", 10, 35, value = 20),
+#                         checkboxInput("showModel1", "Show/Hide Model 1", value = TRUE),
+#                         checkboxInput("showModel2", "Show/Hide Model 2", value = TRUE),
+#                         submitButton("Submit")
+#                 ),
+#                 mainPanel(
+#                         plotOutput("plot1"),
+#                         h3("Predicted Horsepower from Model 1:"),
+#                         textOutput("pred1"),
+#                         h3("Predicted Horespower from Model 2:"),
+#                         textOutput("pred2")
+#                 )
+#         )
+# ))
+
+# #Tabs
+# library(shiny)
+# shinyUI(fluidPage(
+#         titlePanel("Tabs!"),
+#         sidebarLayout(
+#                 sidebarPanel(
+#                         textInput("box1", "Enter Tab 1 Text:", value = "Tab 1!"),
+#                         textInput("box2", "Enter Tab 2 Text:", value = "Tab 2!"),
+#                         textInput("box3", "Enter Tab 3 Text:", value = "Tab 3!")
+#                 ),
+#                 mainPanel(
+#                         tabsetPanel(type = "tabs",
+#                                     tabPanel("Tab 1", br(), textOutput("out1")),
+#                                     tabPanel("Tab 2", br(), textOutput("out2")),
+#                                     tabPanel("Tab 3", br(), textOutput("out3"))
+#                                         )
+#                                 )
+#                 )
+#         )
+# )
+
+#Interactive Graphics
 library(shiny)
 shinyUI(fluidPage(
-        titlePanel("Plot Random Numbers"),
+        titlePanel("Visualize Many Models"),
         sidebarLayout(
                 sidebarPanel(
-                        numericInput("numeric", "How Many Random Numbers Should be Plotted?",
-                                     value = 1000, min = 1, max = 1000, step = 1),
-                        sliderInput("sliderX", "Pick Minimum and Maximum X Values",
-                                    -100, 100, value = c(-50, 50)),
-                        sliderInput("sliderY", "Pick Minimum and Maximum Y Values",
-                                    -100, 100, value = c(-50, 50)),
-                        checkboxInput("show_xlab", "Show/Hide X Axis Label", value = TRUE),
-                        checkboxInput("show_ylab", "Show/Hide Y Axis Label", value = TRUE),
-                        checkboxInput("show_title", "Show/Hide Title")
+                        h3("Slope"),
+                        textOutput("slopeOut"),
+                        h3("Intercept"),
+                        textOutput("intOut")
                 ),
                 mainPanel(
-                        h3("Graph of Random Points"),
-                        plotOutput("plot1")
+                        plotOutput("plot1", brush = brushOpts(
+                                id = "brush1"
+                        ))
                 )
         )
 ))
